c6d8b77348e7d7623f272eb7c1380ef91bd7e515
ch <mkmsacc@outlook.com>
2020-08-17 21:31:34 +0700

    first commit

---
+++ resources/js/components/Count.vue
@@ -0,0 +1,44 @@
+<template>
+  <span v-text="count"></span>
+</template>
+
+<script>
+export default {
+  props: ["to"],
+  data() {
+    return { count: 0, interval: null };
+  },
+  computed: {
+    increment() {
+      return Math.ceil(this.to / 30);
+    }
+  },
+  methods: {
+    tick() {
+      if (this.count + this.increment >= this.to) {
+        this.count = this.to;
+        return clearInterval(this.interval);
+      }
+
+      this.count += this.increment;
+    }
+  },
+  mounted() {
+    const toBeShown = this.$el;
+    console.log(toBeShown);
+
+    const options = {};
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+          this.interval = setInterval(this.tick, 40);
+        }
+      });
+    }, options);
+
+    observer.observe(toBeShown);
+  }
+};
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="app">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <count to="200000" id="test"></count>
+
+    </div>
+
+
+    <script src="/js/app.js"></script>
+</body>
+
+</html>
